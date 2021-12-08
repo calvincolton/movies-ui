94d059ff12657c6152c6b2ea0cc9578cbab49112
Calvin Colton <calvincolton@gmail.com>
2021-12-08 11:38:59 -0800

    fetch movies and movie

--- src/components/MovieDetails/MovieDetails.js
+++ src/components/MovieDetails/MovieDetails.js
@@ -4,16 +4,32 @@ import axios from "axios";
 
 const MovieDetails = () => {
   const { id } = useParams();
+  const [loading, setLoading] = useState(false);
   const [movie, setMovie] = useState({});
+  const [error, setError] = useState(null);
 
-  useEffect(async () => {
-    const res = await axios.get(`www.movies.com/api/movies/${id}`);
-    setMovie(res);
+  useEffect(() => {
+    const fetchMovieDetails = async () => {
+      try {
+        const res = await axios.get(`http://localhost:4000/v1/movies/${id}`);
+        setMovie(res.data.movie);
+      } catch (e) {
+        setError(new Error(e).message);
+      }
+    };
+
+    setLoading(true);
+    fetchMovieDetails();
+    setLoading(false);
   }, [id]);
+
+  if (error) {
+    return <div className="movie-details">{error}</div>;
+  }
 
   return (
     <div className="movie-details">
-      <h2>{movie.title}</h2>
+      {loading ? <div>Loading...</div> : <h2>{movie.title}</h2>}
     </div>
   );
 };
--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,27 +1,44 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 const Movies = () => {
+  const [loading, setLoading] = useState(false);
   const [movies, setMovies] = useState([]);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    setMovies([
-      { id: 1, title: "Shawshank" },
-      { id: 2, title: "Godfather" },
-      { id: 3, title: "Jurassic P" },
-    ]);
+    const fetchMovies = async () => {
+      try {
+        const res = await axios.get("http://localhost:4000/v1/movies");
+        setMovies(res.data.movies);
+      } catch (e) {
+        setError(new Error(e).message);
+      }
+    };
+    setLoading(true);
+    fetchMovies();
+    setLoading(false);
   }, []);
+
+  if (error) {
+    return <div className="movies row">{error}</div>;
+  }
 
   return (
     <div className="movies row">
       <h2>Movies</h2>
-      <ul>
-        {movies.map(({ id, title }) => (
-          <li key={id}>
-            <Link to={`/movies/${id}`}>{title}</Link>
-          </li>
-        ))}
-      </ul>
+      {loading ? (
+        <div>Loading...</div>
+      ) : (
+        <ul>
+          {movies.map(({ id, title }) => (
+            <li key={id}>
+              <Link to={`/movies/${id}`}>{title}</Link>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
